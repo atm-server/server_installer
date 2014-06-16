c3b4b7803f84b702f2495f2f88018d24cc9d460f
root <root@atmc-serveurtest>
2014-06-16 10:35:36 +0200

    add clones 1rst

---
+++ 03_clones.sh
@@ -0,0 +1,7 @@
+mkdir /var/www/_default
+cd /var/www/_default
+git clone git@github.com:ATM-Consulting/dolibarr.git
+cd /var/www/_default/dolibarr/ & git checkout 3.5
+mkdir /var/www/_default/dolibarr/htdocs/custom
+cd /var/www/_default/dolibarr/htdocs/custom
+
