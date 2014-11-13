f352cd425b6980ebaca7597d6cdabd642e6ae444
root <root@lin-01.(none)>
2014-11-13 10:50:56 +0100

    update suite newweb

--- 02_sshkey.sh
+++ 02_sshkey.sh
@@ -1,2 +1,11 @@
+if [ ! $# == 1 ]; then
+        echo "Il faut 1 paramètre pour l'IP du serveur 1"
+        exit 0;
+fi
+
+echo "$1  atmsrv1" >> /etc/hosts
+
+
 ssh-keygen
 cat ~/.ssh/id_rsa.pub
+echo "Il faut ajouter cette clef sur les dépôts"
--- 03_clones.sh
+++ 03_clones.sh
@@ -1,7 +1,13 @@
 mkdir /home/_default
+mkdir /home/client
+
 cd /home/_default
 git clone git@github.com:ATM-Consulting/dolibarr.git
-cd /home/_default/dolibarr/ & git checkout 3.5
+
+cd /home/_default/dolibarr/ 
+git checkout 3.6
+git pull
+
 mkdir /home/_default/dolibarr/htdocs/custom
 cd /home/_default/dolibarr/htdocs/custom
 
@@ -9,7 +15,4 @@ git clone git@atmsrv1:doli-titre subtotal
 git clone git@atmsrv1:doli-docpreview docpreview
 git clone git@atmsrv1:doli-abricot abricot
 
-ln -s /home/_default /var/www/_default
 
-
-mkdir /home/client
