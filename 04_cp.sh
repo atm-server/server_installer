eb00636b34e314c893d0533ead0a114499aebfae
root <root@lin-01.(none)>
2014-11-13 10:51:24 +0100

    ajout

---
+++ 04_cp.sh
@@ -0,0 +1,20 @@
+cd ~
+
+cp auto_install.sh /var/www/
+cp dump-all.sh /home/client/
+cp pull_all.sh /home/_default/dolibarr/htdocs/custom/
+cp auto_install.sh /var/www/
+cp dolibarr_defaut /etc/apache2/sites-available
+
+#write out current crontab
+crontab -l > mycron
+#echo new cron into cron file
+echo "0	0	*	*	*	/home/client/dump-all.sh" >> mycron
+echo "30      12       *       *       *       /home/client/dump-all.sh" >> mycron
+#install new cron file
+crontab mycron
+rm mycron
+
+cd /var/www
+ln -s /home/_default
+ln -s /home/client
