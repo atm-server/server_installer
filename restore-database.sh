7f1f7e754f8caa842dbf624ec2cca442a6c0de11
root <root@localsrv1>
2015-07-02 16:29:06 +0200

    NEW restore data script

---
+++ restore-database.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+exit
+#commenter exit pour vraiment mettre ce truc en place c'est dangerous !
+#export LANG=en_us_8859_1
+cd /var/www
+echo 'Extraction...'
+tar xzf database_`date +"%a-00"`.sql.tar.bz2
+echo 'Chargement...'
+mysql -uroot -p'root_password' < database_`date +"%a-00"`.sql
+echo 'Fin...'
+## Suppression des exports non compresses
+rm database_`date +"%a-00"`.sql
+rm database_`date +"%a-00"`.sql.tar.bz2
+
+
