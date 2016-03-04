bbb713cd7b8ac55b4acb65b47a2a82056e56cc3d
Alexis ALGOUD <alexis@atm-consulting.fr>
2016-03-04 17:35:37 +0100

    Create 06_make_swap_if_not_exist.sh

---
+++ 06_make_swap_if_not_exist.sh
@@ -0,0 +1,5 @@
+#dd if=/dev/zero of=/mnt/512MiB.swap bs=1024 count=524288
+dd if=/dev/zero of=/mnt/file.swap bs=1024 count=1048576
+chmod 600 /mnt/file.swap
+mkswap /mnt/file.swap
+swapon /mnt/file.swap 
