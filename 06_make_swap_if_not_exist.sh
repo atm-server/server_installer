912d56d694fbaa9fd8a4ffd33614034c3e902e33
Alexis ALGOUD <alexis@atm-consulting.fr>
2016-03-04 17:43:51 +0100

    Update 06_make_swap_if_not_exist.sh

--- 06_make_swap_if_not_exist.sh
+++ 06_make_swap_if_not_exist.sh
@@ -3,4 +3,4 @@ dd if=/dev/zero of=/mnt/file.swap bs=1024 count=1048576
 chmod 600 /mnt/file.swap
 mkswap /mnt/file.swap
 swapon /mnt/file.swap 
-echo "/mnt/file.swap none swap sw 0 0" > /etc/fstab
+echo "/mnt/file.swap none swap sw 0 0" >> /etc/fstab
