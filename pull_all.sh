5519093307ce676f452be3ce19b6548789d33db8
phf <phf@atm-consulting.fr>
2016-05-25 15:29:22 +0200

    UPDATE une pointe de couleur quand on utilise le pull_all.sh pour bien repérer dans le terminal le début d'un git pull

--- pull_all.sh
+++ pull_all.sh
@@ -1,8 +1,12 @@
+LIGHT_BLUE='\033[1;34m'
+WHITE='\033[1;37m'
+LIGHT_RED='\033[1;31m'
+NC='\033[0m'
 
 ListeRep="$(find * -type d -prune)"   # liste des repertoires sans leurs sous-repertoires
 for Rep in ${ListeRep}; do
 	cd ./${Rep}/
-	echo ---------- Pulling repo for ${Rep} ----------
+	echo -e ${WHITE}---------- ${LIGHT_BLUE}Pulling ${WHITE}repo for${LIGHT_RED} ${Rep} ${WHITE}----------${NC}
 	git pull
 	cd ..
 done
