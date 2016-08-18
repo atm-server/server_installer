ef59b488660080373637afa09d99fa64e2c741c1
Maxime Kohlhaas <maxime@atm-consulting.fr>
2016-08-18 15:22:20 +0200

    replace deprecated git-core by git package

--- 05b_extract_defaut_db.sh
+++ 05b_extract_defaut_db.sh
@@ -1,6 +1,6 @@
 mysqldump -u root -p defaut_dolibarr > /var/www/defaut_dolibarr.sql
 cd /var/www/
-tar -zcvf ~/server_installer/d.tar  defaut_dolibarr.sql
+tar -zcvf ~/server_installer/d.tar defaut_dolibarr.sql
 cd ~/server_installer
 openssl aes-256-cbc -in d.tar -out d.enc
 rm d.tar
