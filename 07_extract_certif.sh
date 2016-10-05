1a26e936890c5b31095e19729a4643e396be9dc8
root <root@mail.srv16.atm-consulting.fr>
2016-10-05 17:00:10 +0200

    NEW include certbot

--- 07_extract_certif.sh
+++ 07_extract_certif.sh
@@ -1,6 +1,7 @@
 cd ~/server_installer
 
-openssl aes-256-cbc -d -in k.enc | tar xz
+#openssl aes-256-cbc -d -in k.enc | tar xz
 mkdir /home/ssl-cert
-mv srv8.atm-consulting.fr.* /home/ssl-cert/
+#mv srv8.atm-consulting.fr.* /home/ssl-cert/
 
+cp  certbot-auto /home/ssl-cert/
