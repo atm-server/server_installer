8128e8933ff0c26887880ce65806fd76b6c0af6b
Serveur ATM <alexis@atm-consulting.fr>
2017-11-29 09:53:55 +0100

    improve dump all to just prod database

--- dump-all.sh
+++ dump-all.sh
@@ -4,7 +4,9 @@ cd /home/client
 
 rm database_`date +"%a-%H"`.sql.tar.bz2 
 
-mysqldump -uroot --password="your-db-password" --all-databases --add-drop-table > database_`date +"%a-%H"`.sql
+#mysqldump -uroot --password="your-db-password" --all-databases --add-drop-table > database_`date +"%a-%H"`.sql
+
+mysql  -uroot --password="your-db-password" -N -e "show databases like '%_dolibarr';" | grep -v -F databaseofoldclient1 | grep -v -F databaseofoldclient2 | xargs mysqldump -uroot --password="your-db-password" --add-drop-table --databases > database_`date +"%a-%H"`.sql
 
 tar jcf database_`date +"%a-%H"`.sql.tar.bz2 database_`date +"%a-%H"`.sql
 
