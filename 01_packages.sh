1b3bb62a4bf944b452066ae9a6b49b17f7ddd56c
Votre Nom <Vous@exemple.com>
2018-06-19 10:07:35 +0200

    echo change root mariadb

--- 01_packages.sh
+++ 01_packages.sh
@@ -1,3 +1,10 @@
 apt-get update
 apt-get install apache2 php mariadb-client mariadb-server libapache2-mod-php php-mysql phpmyadmin git php-curl libreoffice-writer postfix apache2-utils ntp bzip2 locate bash-completion fail2ban vim
 a2enmod ssl
+
+echo "mysql -u root -p"
+echo "Enter Password : (vous tapez entrer)"
+ 
+echo "MariaDB [(none)]> UPDATE mysql.user SET plugin = NULL WHERE user = 'root' AND plugin = 'unix_socket';"
+echo "MariaDB [(none)]> FLUSH PRIVILEGES;"
+echo "MariaDB [(none)]> SET PASSWORD FOR root@'localhost'=PASSWORD('mon_nouveau_mot-de_passe');"
