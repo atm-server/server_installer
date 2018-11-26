ce880e4b5fb71c51cc8cd20d191244bd03b05383
FAVRE Pierre-Henry <phf@atm-consulting.fr>
2018-11-26 17:46:38 +0100

    Update 03_clones.sh

--- 03_clones.sh
+++ 03_clones.sh
@@ -4,7 +4,7 @@ mkdir /home/client
 touch /home/client/.htpasswd
 
 cd /home/_default
-git clone git@github.com:ATM-Consulting/dolibarr.git --branch 6.0 
+git clone git@github.com:ATM-Consulting/dolibarr.git --branch 8.0 
 
 cd /home/_default/dolibarr/ 
 git pull
